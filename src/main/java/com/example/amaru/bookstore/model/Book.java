78c014f50c287a135197c051d306a8dc18dc46bf
amarukorp <amarucak@gmail.com>
2022-02-02 14:59:24 +0200

    first commit

---
+++ src/main/java/com/example/amaru/bookstore/model/Book.java
@@ -0,0 +1,12 @@
+package com.example.amaru.bookstore.model;
+
+public class Book {
+	// Attributes 
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+	
+	
+}
