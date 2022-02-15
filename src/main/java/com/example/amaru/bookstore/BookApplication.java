4c01e2ee15fba3872cfa72053154fc53d0a8fc03
amarukorp <amarucak@gmail.com>
2022-02-16 00:07:26 +0200

    Added category entity with category repo, added functionality to connect both entities, and made the previous features work with the new ones

--- src/main/java/com/example/amaru/bookstore/BookApplication.java
+++ src/main/java/com/example/amaru/bookstore/BookApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.amaru.bookstore.model.Book;
 import com.example.amaru.bookstore.model.BookRepository;
+import com.example.amaru.bookstore.model.Category;
+import com.example.amaru.bookstore.model.CategoryRepository;
 
 @SpringBootApplication
 public class BookApplication {
@@ -16,11 +18,16 @@ public class BookApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
+			System.out.println("save a couple of categories");
+			crepository.save(new Category("Economy"));
+			crepository.save(new Category("Self-improvement"));
+			crepository.save(new Category("Coding"));
+			
 			System.out.println("save a couple of students");
-			repository.save(new Book("What money can't buy: the moral limits of markets", "Michael Sandel", 2012, "9781846144714", 15.00));
-			repository.save(new Book("How to win friends and influence people", "Dale Carnegie", 1998, "0671027034", 15.00));
+			repository.save(new Book("What money can't buy: the moral limits of markets", "Michael Sandel", 2012, "9781846144714", 15.00, crepository.findByName("Economy").get(0) ));
+			repository.save(new Book("How to win friends and influence people", "Dale Carnegie", 1998, "0671027034", 15.00, crepository.findByName("Self-improvement").get(0) ));
 		
 			System.out.println("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/com/example/amaru/bookstore/web/BookController.java
+++ src/main/java/com/example/amaru/bookstore/web/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.amaru.bookstore.model.Book;
 import com.example.amaru.bookstore.model.BookRepository;
+import com.example.amaru.bookstore.model.CategoryRepository;
 
 @Controller
 
@@ -15,6 +16,9 @@ public class BookController {
 	
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository;
 	
 	@RequestMapping(value = {"/", "/booklist"})
 	public String listBooks(Model model) {
@@ -25,6 +29,7 @@ public class BookController {
     @RequestMapping(value = "/add")
     public String addBook(Model model){
     	model.addAttribute("book", new Book());
+    	model.addAttribute("categories", crepository.findAll());
         return "addbook";
     } 
     
@@ -42,6 +47,7 @@ public class BookController {
     @RequestMapping(value = "/edit/{id}", method= RequestMethod.GET)
     public String editBook (@PathVariable("id") Long id, Model model) {
     	model.addAttribute("book", repository.findById(id));
+    	model.addAttribute("categories", crepository.findAll());
     	return "editbook";
     }
     
